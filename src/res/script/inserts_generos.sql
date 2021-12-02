920bfa6e7c7a9e4ab87acc39b4ee0199bfc7466d
Richard Roling <richard.a.roling@gmail.com>
2021-12-02 15:48:49 -0300

    Criação de novo arquivo para inserts de generos

---
+++ src/res/script/inserts_generos.sql
@@ -0,0 +1,4 @@
+insert into generos (label) values ('Feminino');
+insert into generos (label) values ('Masculino');
+insert into generos (label) values ('Outro');
+insert into generos (label) values ('Prefiro não informar');
